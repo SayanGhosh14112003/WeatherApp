ddf8f95af0e3961ad945024fef992fe364c9f5e2
SAYAN GHOSH <97933977+SayanGhosh14112003@users.noreply.github.com>
2023-07-29 01:44:29 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,7 @@
 document.getElementById("btn").addEventListener("click", function (event) {
   event.preventDefault()
 });
+var marker=L.marker([0,0]);
 function c(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -13,9 +14,10 @@ L.tileLayer('https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-tok
 async function main(city) {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=886705b4c1182eb1c69f28eb8c520e20`;
   try{
-        const response = await fetch(url);
+    const response = await fetch(url);
+         map.removeLayer(marker)
         if (response.status === 404) {
-          throw new Error('Invalid Location');
+          throw new Error('Location not found!');
         } else if (response.status === 500) {
           throw new Error('Server error');
         } else if (!response.ok) {
@@ -24,10 +26,24 @@ async function main(city) {
         const data = await response.json();
         let image = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
         map.setView([data.coord.lat, data.coord.lon], 10);
-        var marker = new L.marker([data.coord.lat, data.coord.lon]).bindPopup("<img src=" + image + "><span><h3>" + c(data.weather[0].description) + "</h3></span><hr><h2>" + data.name+"("+data.sys.country+")"+ "</h2><h4>Temperature: " + (data.main.temp - 273).toFixed(1) + "°C</h4><h4>Humidity: " + data.main.humidity + "%</h4><h4>Wind: " + (data.wind.speed * 3.6).toFixed(1) + " Km/h</h4>")
+        marker =  L.marker([data.coord.lat, data.coord.lon]).bindPopup("<img src=" + image + "><span><h3>" + c(data.weather[0].description) + "</h3></span><hr><h2>" + data.name+"("+data.sys.country+")"+ "</h2><h4>Temperature: " + (data.main.temp - 273).toFixed(1) + "°C</h4><h4>Humidity: " + data.main.humidity + "%</h4><h4>Wind: " + (data.wind.speed * 3.6).toFixed(1) + " Km/h</h4>")
         marker.addTo(map).openPopup();
     }catch(err){
       alert(err);
+    };
+}
+async function main2(lat,lng) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=886705b4c1182eb1c69f28eb8c520e20`;
+  try{
+    const response = await fetch(url);
+    map.removeLayer(marker)
+        const data = await response.json();
+        let image = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+        map.setView([data.coord.lat, data.coord.lon], 10);
+        marker = L.marker([data.coord.lat, data.coord.lon]).bindPopup("<img src=" + image + "><span><h3>" + c(data.weather[0].description) + "</h3></span><hr><h2>" + data.name+"("+data.sys.country+")"+ "</h2><h4>Temperature: " + (data.main.temp - 273).toFixed(1) + "°C</h4><h4>Humidity: " + data.main.humidity + "%</h4><h4>Wind: " + (data.wind.speed * 3.6).toFixed(1) + " Km/h</h4>")
+        marker.addTo(map).openPopup();
+    }catch(err){
+      console.log(err);
     };
 }
 btn.addEventListener('click', async() => {
@@ -35,4 +51,12 @@ btn.addEventListener('click', async() => {
     main(search.value);
   }catch(err){}
 })
+
+map.on('click', function(e){
+  var coord = e.latlng;
+  var lat = coord.lat;
+  var lng = coord.lng;
+  console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+  main2(lat,lng)
+  });
 
